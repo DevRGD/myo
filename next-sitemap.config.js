0b02a2e7e27a832ad89c83168d8c5ee9fdd4a41b
DevRGD <gautm.das@gmail.com>
2025-04-30 01:48:55 +0530

    replaced favicon

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,22 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://manage-your-office.vercel.app',
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
   sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: ['/admin', '/404'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      // Optional if you use separate sitemaps like blog or dynamic
+      // 'https://devrgd.space/server-sitemap.xml',
+    ],
+  },
 };
